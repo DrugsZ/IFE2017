b1970abf7ee72956412e15a59188c7609539c215
DrugsZ <1002881508@qq.com>
2017-12-24 19:12:56 +0800

    修改打包配置

--- 听指令的小方块/webpack.config.js
+++ 听指令的小方块/webpack.config.js
@@ -4,7 +4,8 @@
 module.exports = {
     entry: __dirname + "/app/main.js",
     output: {
-        path: __dirname + "/public",
+        // path: __dirname + "/public",
+        path:"/", 
         filename: "bundle.js"
     },
     module: {
