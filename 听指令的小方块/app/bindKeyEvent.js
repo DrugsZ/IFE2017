729417f51e32d2e938251308d1195f8ff7eeb069
DrugsZ <1002881508@qq.com>
2017-12-26 22:19:16 +0800

    打算把这个改成工具类模块

--- 听指令的小方块/app/bindKeyEvent.js
+++ 听指令的小方块/app/bindKeyEvent.js
@@ -1,14 +1,9 @@
-export default{
-	addHandler(ele,event,handler){
-	    if(ele.attachEvent){
-	        ele.attachEvent("on"+event,handler);
-	    }else if(ele.addEventListener){
-	        ele.addEventListener(event,handler,false);
-	    }else{
-	        ele[on+"event"]=handler;
-	    }
-	},
-	bindKey(){
-		
+export function addHandler(ele,event,handler){
+	if(ele.attachEvent){
+		ele.attachEvent("on"+event,handler);
+	}else if(ele.addEventListener){
+		ele.addEventListener(event,handler,false);
+	}else{
+		ele[on+"event"]=handler;
 	}
 }
