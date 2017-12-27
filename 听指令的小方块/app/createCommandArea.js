3570bf230f01e270182a8dd3cebf051be436207c
DrugsZ <1002881508@qq.com>
2017-12-27 16:55:47 +0800

    ..

--- 听指令的小方块/app/createCommandArea.js
+++ 听指令的小方块/app/createCommandArea.js
@@ -1,26 +1,59 @@
+/*
+ * @Author: DrugsZ 
+ * @Date: 2017-12-27 11:51:42 
+ * @Last Modified by:   DrugsZ 
+ * @Last Modified time: 2017-12-27 11:51:42 
+ */
 import {addHandler} from "./util.js"
-export function createCommandDiv(){
-        let parentDiv = document.createElement('div');
-        parentDiv.className = 'command';
-        let subDiv = document.createElement('div');
-        subDiv.className = "lineNumber"
-        parentDiv.appendChild(subDiv)
-        let textarea = document.createElement('textarea');
-        textarea.className = 'line_height'
-        // textarea.rows = 40;
-        // textarea.cols = 40;
-        parentDiv.appendChild(textarea);
-        document.body.appendChild(parentDiv)
+export function initCommand(){
+	createCommandDiv()
+}
+function createCommandDiv(){
+    let parentDiv = document.createElement('div');
+    parentDiv.className = 'command';
+    let subDiv = document.createElement('div');
+    subDiv.className = "lineNumber"
+    parentDiv.appendChild(subDiv)
+    let textarea = document.createElement('textarea');
+    textarea.className = 'line_height'
+    parentDiv.appendChild(textarea);
+	document.body.appendChild(parentDiv)
+	let getLineNumber = isLineFeed(textarea)
+	let createNumber = getNumber(subDiv)
+	addHandler(textarea,'keyup',function(){
+		let n = getLineNumber()
+		if(n>0){
+			createNumber(n)
+		}	
+	})
+}
+function isLineFeed(el){
+	return function getLineNumber(){
+		let value = el.value;
+		console.log(value)
+		let n = value.split('\n')
+		// console.log(n)
+		return n.length
+	}
 }
 function getNumber(target){
-        let index = 1
-        return function createNumber(){
-                let oDiv = document.createElement('div');
-                oDiv.className = "number";
-                let span = document.createElement('span');
-                span.innerHTML = index;
-                oDiv.appendChild(span)
-                target.appendChild(oDiv)
-                index+=1
-        }
+	let lastIndex
+    return function createNumber(index){
+		// console.log(index)
+		if(lastIndex==index)return;
+		lastIndex = index
+		let child = target.querySelectorAll('span');
+        let oDiv = document.createElement('div');
+        oDiv.className = "number";
+        let span = document.createElement('span');
+        span.innerHTML = index;
+        oDiv.appendChild(span)
+        target.appendChild(oDiv)
+    }
+}
+/*
+* @params:object
+*/
+function receiveCompleted(){
+
 }
--- 听指令的小方块/app/init.js
+++ 听指令的小方块/app/init.js
@@ -1,13 +1,13 @@
     import setMap from "./createMap.js"
     import {CreateCar} from "./createCar.js"
-    import { createCommandDiv} from "./createCommandArea.js"
+    import { initCommand} from "./createCommandArea.js"
 
 	export function init(){
         let map = document.getElementById('map')
         let table = setMap.setMap()
         map.appendChild(table)
         let size = table.childNodes.length
-        createCommandDiv()
+        initCommand()
         map.appendChild(table)
         window.car1 = new CreateCar()
         car1.setMapSize(size)
