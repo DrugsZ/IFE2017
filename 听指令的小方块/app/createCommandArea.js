67e3d888f7dfea76af85e61cbbfdb51850f15097
DrugsZ <1002881508@qq.com>
2018-01-17 15:37:27 +0800

    重写命令模块

--- 听指令的小方块/app/createCommandArea.js
+++ 听指令的小方块/app/createCommandArea.js
@@ -1,59 +1,57 @@
-/*
- * @Author: DrugsZ 
- * @Date: 2017-12-27 11:51:42 
- * @Last Modified by:   DrugsZ 
- * @Last Modified time: 2017-12-27 11:51:42 
- */
-import {addHandler} from "./util.js"
-export function initCommand(){
-	createCommandDiv()
-}
-function createCommandDiv(){
-    let parentDiv = document.createElement('div');
-    parentDiv.className = 'command';
-    let subDiv = document.createElement('div');
-    subDiv.className = "lineNumber"
-    parentDiv.appendChild(subDiv)
-    let textarea = document.createElement('textarea');
-    textarea.className = 'line_height'
-    parentDiv.appendChild(textarea);
-	document.body.appendChild(parentDiv)
-	let getLineNumber = isLineFeed(textarea)
-	let createNumber = getNumber(subDiv)
-	addHandler(textarea,'keyup',function(){
-		let n = getLineNumber()
-		if(n>0){
-			createNumber(n)
-		}	
-	})
-}
-function isLineFeed(el){
-	return function getLineNumber(){
-		let value = el.value;
-		console.log(value)
-		let n = value.split('\n')
-		// console.log(n)
-		return n.length
-	}
-}
-function getNumber(target){
-	let lastIndex
-    return function createNumber(index){
-		// console.log(index)
-		if(lastIndex==index)return;
-		lastIndex = index
-		let child = target.querySelectorAll('span');
-        let oDiv = document.createElement('div');
-        oDiv.className = "number";
-        let span = document.createElement('span');
-        span.innerHTML = index;
-        oDiv.appendChild(span)
-        target.appendChild(oDiv)
-    }
-}
-/*
-* @params:object
-*/
-function receiveCompleted(){
+import {addHandler} from './util.js'
 
+let WRAPPER;
+let VALUE;
+let LAST_NUM;
+let COMMAND;
+
+export function initCommand(){
+    let commandStr =`<div class="command">
+                    <div class="lineNumber">
+                        <div class="wrapper">
+                            
+                        </div>
+                    </div>
+                    <textarea></textarea>
+                </div>`
+    document.body.innerHTML+=commandStr
+    WRAPPER = document.querySelector('.wrapper');
+    COMMAND = document.querySelector('.command')
+    setTimeout(function(){
+        addNumber(1)
+        addHandler(COMMAND,'keyup',function(){
+            console.log(121)
+            renderLine(getLineNumber())
+        })
+    },0)
+}
+
+function renderLine(num){
+    if(LAST_NUM==num){
+        return;
+    }
+    LAST_NUM = num;
+    addNumber(num)
+}
+
+function addNumber(i){
+    let str ='';
+    for (var n = 0; n < i; n++) {
+        str+=createNumber(n+1)
+    }
+    WRAPPER.innerHTML =str
+}
+
+function createNumber(i){
+    let str = `<div>
+                    <span>${i}</span>
+                </div>`
+    return str
+}
+
+function getLineNumber(){
+    VALUE = document.querySelector('textarea').value;
+    let n = VALUE.split('\n').length
+    console.log(n)
+    return n;
 }
